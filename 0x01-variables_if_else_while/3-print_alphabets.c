bceebc3ae2a75567dc18abce4141e1afa228e36d
root <lesleymukami@gmail.com>
2022-03-14 16:12:27 -0700

    task3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,17 +1,16 @@
 #include<stdio.h>
 Int main(void)
 {
-char a = ‘a’;
-char A = ‘A’;
-While (a <= ‘z’)
- { 
-	putchar(a);
-	a++
- }
-while (A <= Z)
-{
-	putchar(A);
-	A++
-}
-return 0;
+	char c = ‘a’;
+
+	for (c='a', c<= ‘z', c++)
+	{ 
+	putchar(c);
+	}
+	for (c = 'A', c<= 'Z', c++)
+	{
+		putchar(c);
+	}
+	putchar('\n')
+	return 0;
 }
