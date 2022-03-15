7509232ce03897381355347e607efd758dfdbe13
root <lesleymukami@gmail.com>
2022-03-14 17:23:04 -0700

    task03

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
 int main(void)
 {
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-	{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
 	putchar(c);
-	}
-	for (c = 'A'; c <= 'Z'; c++)
-	{
-		putchar(c);
-	}
-	putchar('\n');
-	return (0);
+}
+for (c = 'A'; c <= 'Z'; c++)
+{
+	putchar(c);
+}
+putchar('\n');
+return (0);
 }
