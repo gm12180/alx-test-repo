57adc272a07dfa0fb3b85425c03adc366d0f1835
root <lesleymukami@gmail.com>
2022-03-15 18:54:10 -0700

    task110

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,7 +2,7 @@
 /**
  * add - add two numbers.
  * @i: The first number to be added.
- * @j: The second number to be added. 
+ * @j: The second number to be added.
  * Return: the sum of two numbers.
  */
 int add(int i, int j)
