bcfd81573a5d5c5837f73bd370ab7cea6cca5a47
root <lesleymukami@gmail.com>
2022-03-15 19:13:10 -0700

    task3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,10 +6,17 @@
  * Return: 1 if it is a lowercase letter , 0 if not lowercase
  */
 int _islower(int c)
+
 {
-	if (c >= 'a' && c <= 'z')
+
+	if (c > 'a' && c < 'z')
+	{
 		return (1);
+	}
+
 	else
+	{
 		return (0);
+	}
 
 }
